31db237dc91956ea12cdd3c58fe92b5459464408
Chris Claxton <chrisclaxton@hotmail.co.uk>
2018-10-30 19:48:40 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# webpack-starter-basic
+
+This was created from the following repo:
+https://github.com/lifenautjoe/webpack-starter-basic
+
+The instructions in the repo readme were followed, specifically:
+
+```sh
+ npm install 
+ npm run kickstart
+```
+
+I then modified the webpack configs to use an output.publicPath so that the content can be served from some sub-path off the root of a domain.
+
+However, when using a public path of '/app' (without a trailing slash) it causes the file-loader plugin to concatenate the options.publicPath with the value of options.name without using a '/' path separator.
+
+To observe this run the following command (uses webpack.dev.js)
+```sh
+npm start
+```
+
+Open up Chrome dev tools > Source and notice that the file 
+`logo-on-dark-bg.png` has been placed under an incorrect  path of 'appsrc/assets'.
+
+The correct path should be 'app/src/assets'
+
+To fix the issue either correct the publicPath to '/app/' or uncomment the `//publicPath: baseUrl` on line 70 in the webpack config
+
+
---
+++ webpack.dev.js
@@ -0,0 +1,83 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const buildPath = path.resolve(__dirname, 'dist');
+const baseUrl = '/app';
+
+module.exports = {
+    devtool: 'eval-cheap-module-source-map',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[hash:20].js',
+        path: buildPath,
+        publicPath: baseUrl
+    },
+    devServer: {
+        port: 8080,
+        contentBase: buildPath
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    {
+                        // creates style nodes from JS strings
+                        loader: "style-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        // translates CSS into CommonJS
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        // compiles Sass to CSS
+                        loader: "sass-loader",
+                        options: {
+                            outputStyle: 'expanded',
+                            sourceMap: true,
+                            sourceMapContents: true
+                        }
+                    }
+                    // Please note we are not running postcss here
+                ]
+            }
+            ,
+            {
+                // Load all images as base64 encoding if they are smaller than 8192 bytes
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader', // loader: 'url-loader',
+                        options: {
+                            // On development we want to see where the file is coming from, hence we preserve the [path]
+                            name: '[path][name].[ext]?hash=[hash:20]',
+                            //limit: 8192
+                            //publicPath: baseUrl
+                        }
+                    }
+                ]
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            inject: true
+        })
+    ]
+};
